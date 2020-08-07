37ae7b0e48db4ec05472d9612d17d1172459d0c7
Beatriz Murase <biamurase@gmail.com>
2020-08-07 18:10:35 -0300

    feat: include children

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -12,7 +12,7 @@ interface PageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
   const { navigate } = useNavigation();
   return (
     <View style={styles.container}>
@@ -25,6 +25,8 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
       </View>
 
       <Text style={styles.title}>{title}</Text>
+
+      {children}
     </View>
   );
 };
